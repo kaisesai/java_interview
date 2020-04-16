4079b7bd15ead370eb81c42415cd1f9afe871898
liukai <liukai@yzyw-inc.com>
2020-04-16 16:20:06 +0800

    feat: BIO

--- src/main/java/com/liukai/javainterview/nio/BIOServer.java
+++ src/main/java/com/liukai/javainterview/nio/BIOServer.java
@@ -1,9 +1,10 @@
 package com.liukai.javainterview.nio;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * BIO 服务端
@@ -14,41 +15,121 @@ public class BIOServer {
 
     // 服务端处理客户端连接请求
     ServerSocket serverSocket = new ServerSocket(3333);
-
     // 接收客户端的请求之后，为每一个客户端创建一个新的线程进行链路处理
-    new Thread(() -> doAcceptorClientReq(serverSocket)).start();
-
-    System.out.println("服务端已经启动！");
+    doAcceptorClientReq(serverSocket);
 
   }
 
   private static void doAcceptorClientReq(ServerSocket serverSocket) {
-    while (true) {
-      try {
+    try {
+
+      // 固定数量的线程池
+      ExecutorService executorService = Executors.newFixedThreadPool(100);
+
+      while (true) {
         // 阻塞方法获取新的连接
         Socket socket = serverSocket.accept();
-
-        new Thread(() -> {
-          int len;
-          byte[] bytes = new byte[1024];
+        // 每一个新的连接都创建一个线程，负责读取数据
+        Runnable runnable = () -> {
           try {
-            // 按照字节流方式读取数据
-            InputStream is = socket.getInputStream();
-            while ((len = is.read(bytes)) > 0) {
-              System.out.println("收到客户端数据：" + new String(bytes, 0, len));
-            }
-            socket.shutdownInput();
-            socket.getOutputStream().write("服务器端：你好！".getBytes());
-            socket.shutdownOutput();
-          } catch (IOException ignored) {
+            readWriteReuseSocket(socket);
+          } catch (IOException e) {
+            e.printStackTrace();
           }
-
-        }).start();
-
-      } catch (Exception ignored) {
+        };
+        executorService.submit(runnable);
 
       }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
+
+  /**
+   * 通过指定长度告知对方已发送完命令
+   *
+   * @param socket
+   * @throws IOException
+   */
+  private static void readWriteReuseSocket(Socket socket) throws IOException {
+    // 获取输入流，并建立缓冲区进行读取
+    InputStream is = socket.getInputStream();
+    byte[] bytes;
+    // 因为可以复用 socket且能判断长度，所以可以一个 socket 用到底
+    while (true) {
+      // 首先读取两个字节表示的长度
+      int first = is.read();
+      // 如果读取的值为-1，则说已经到达流的末尾，socket 已经被关闭了，此时将不能再去读取
+      if (first == -1) {
+        break;
+      }
+      // 第二个字节
+      int second = is.read();
+      // 用位运算将两个字节拼起来成为真正的长度
+      int len = (first << 8) + second;
+      // 构建指定大小的 byte 数组
+      bytes = new byte[len];
+      // 读取指定长度的消息
+      is.read(bytes);
+      System.out.println("客户端输入：" + new String(bytes));
+    }
+    is.close();
+    socket.close();
+  }
+
+  /**
+   * 通过约定符号告知对方已发送完命令
+   *
+   * @param socket
+   * @throws IOException
+   * @throws InterruptedException
+   */
+  private static void readWriteFromReader(Socket socket) throws IOException, InterruptedException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    while (true) {
+      // 读取一行数据
+      String readLine = br.readLine();
+      System.out.println("来自客户端消息：" + readLine);
+      // 向客户端写入一行数据
+      bw.write("收到了消息，" + readLine);
+      bw.newLine();
+      bw.flush();
+      // 等待 3秒
+      Thread.sleep(500);
+    }
+  }
+
+  /**
+   * 通过Socket关闭输出流的方式，告知对方已经发送完命令
+   *
+   * @param socket
+   * @throws IOException
+   */
+  private static void readWriteFromStream(Socket socket) throws IOException {
+    InputStream is = socket.getInputStream();
+    OutputStream os = socket.getOutputStream();
+
+    // 读取一行数据
+    byte[] bytes = new byte[1024];
+    int len;
+    StringBuilder sb = new StringBuilder();
+    // 按字节流方式读取数据
+    // 注意 InputStream 的 read 方法会一直阻塞，直到输入有输入数据，文件达到末尾，或者有异常抛出
+    // 当流到达文件的末尾时，返回-1，即流断开之后返回-1
+    while ((len = is.read(bytes)) != -1) {
+      sb.append(new String(bytes, 0, len));
+    }
+    System.out.println("来自客户端消息：" + sb);
+
+    // 服务器端写入消息
+    os.write("我收到你的消息了！".getBytes());
+    // 通过 shutdownOutput 告诉客户端已经发送完消息
+    socket.shutdownOutput();
+
+    is.close();
+    os.close();
+    socket.close();
   }
 
 }
--- src/main/java/com/liukai/javainterview/nio/IOClient.java
+++ src/main/java/com/liukai/javainterview/nio/IOClient.java
@@ -1,9 +1,7 @@
 package com.liukai.javainterview.nio;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
-import java.util.Date;
 
 /**
  * IO 客户端
@@ -14,36 +12,92 @@ public class IOClient {
 
   public static void main(String[] args) {
     // 创建多个线程模拟多个客户端调用
-    new Thread(() -> {
-      try {
-        Socket socket = new Socket("127.0.0.1", 3333);
-        while (true) {
+    // new Thread(() -> {
+    try {
+      Socket socket = new Socket("127.0.0.1", 3333);
+      // readWriteReuseSocket(socket);
+      readWriteFromStream(socket);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    // }).start();
+  }
 
-          // 向服务端写入数据
-          String str = new Date() + ": hello world!";
-          OutputStream os = socket.getOutputStream();
-          os.write(str.getBytes());
-          os.flush();
-          socket.shutdownOutput();
-          System.out.println("客户端输出：" + str);
+  /**
+   * 通过指定长度告知对方已发送完命令
+   *
+   * @param socket
+   * @throws IOException
+   * @throws InterruptedException
+   */
+  private static void readWriteReuseSocket(Socket socket) throws IOException, InterruptedException {
+    OutputStream os = socket.getOutputStream();
+    int i = 0;
+    while (true) {
+      // 向服务端写入数据
+      String msg = "你好啊！" + i++;
+      // 计算得知消息的长度
+      byte[] bytes = msg.getBytes();
+      // 优先发送消息的长度
+      os.write(bytes.length >> 8);
+      os.write(bytes.length);
+      // 再发送消息
+      os.write(bytes);
+      Thread.sleep(2000);
+    }
+  }
 
-          // 从服务端读取数据
-          int len;
-          byte[] bytes = new byte[1024];
-          InputStream is = socket.getInputStream();
-          while ((len = is.read(bytes)) > 0) {
-            System.out.println("服务端返回：" + new String(bytes, 0, len));
-          }
-          // is.close();
-          // socket.shutdownInput();
+  /**
+   * 通过约定符号告知对方已发送完命令
+   *
+   * @param socket
+   * @throws IOException
+   */
+  private static void readWriteFromReaderWriter(Socket socket) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    int i = 0;
+    while (i != 20) {
+      // 向服务端写入数据
+      String line = "你好啊！" + i++;
+      bw.write(line);
+      bw.newLine();
+      bw.flush();
+      // 读取客户端的消息
+      String readLine = br.readLine();
+      System.out.println("收到来自服务器端的消息：" + readLine);
+    }
+  }
 
-          System.out.println("客户端结束会话...");
-          Thread.sleep(2000);
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }).start();
+  /**
+   * 通过Socket关闭输出流的方式，告知对方已经发送完命令
+   *
+   * @param socket
+   * @throws IOException
+   * @throws InterruptedException
+   */
+  private static void readWriteFromStream(Socket socket) throws IOException, InterruptedException {
+
+    OutputStream os = socket.getOutputStream();
+    InputStream is = socket.getInputStream();
+    // 向服务端写入数据
+    String line = "你好啊！";
+    os.write(line.getBytes());
+    // 通过 shutdownOutput 告诉服务器已经发送完数据，后续只能接收数据
+    socket.shutdownOutput();
+
+    StringBuilder sb = new StringBuilder();
+    int len;
+    byte[] bytes = new byte[1024];
+    while ((len = is.read(bytes)) != -1) {
+      sb.append(new String(bytes, 0, len));
+    }
+    System.out.println("服务器端返回：" + sb);
+
+    os.close();
+    is.close();
+    socket.close();
   }
 
 }
+
